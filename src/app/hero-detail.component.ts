e5a6254b70aa76ff45c0ea55d035de3ee96642b5
JorgeZapata95 <jorge_101995@hotmail.com>
2017-03-05 19:40:37 -0500

    Sixth commit: Module 7. Http

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -31,6 +31,14 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+  save(): void {
+  this.heroService.update(this.hero)
+    .then(() => this.goBack());
+  }
+
+}
+
+
 
 
 
